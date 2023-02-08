ed0b4e9dfade1d22e93faa83ce64fc590743c031
ugix-sassiness <78940245+ugix-sassiness@users.noreply.github.com>
2023-02-08 00:05:04 -0600

    added mobile layout

---
+++ member/Controllers/MobileCarbonController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using member.Models;
+
+namespace member.Controllers;
+
+public class MobileController : Controller
+{
+    private readonly ILogger<MobileController> _logger;
+
+    public MobileController(ILogger<MobileController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Messages()
+    {
+        return View();
+    }
+
+    public IActionResult NewServiceAddress()
+    {
+        return View();
+    }
+
+    public IActionResult ReferAFriend()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
