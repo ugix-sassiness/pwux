fb845728f7e5b6bf4a3395cd063b90462e50a77a
ugix-sassiness <78940245+ugix-sassiness@users.noreply.github.com>
2023-03-01 15:54:09 -0600

    added gsap interaction

--- member/wwwroot/js/site.js
+++ member/wwwroot/js/site.js
@@ -6,3 +6,47 @@
     // initialize Swiper
 
 
+// UI Animation
+// Product Menu Navigation
+let t1 = gsap.timeline({ paused: true, reversed: true });
+let watch = false;
+const mode = document.querySelector(".productmenu");
+
+mode.addEventListener("click", (e) => {
+    if (!watch) {
+        t1.play();
+        watch = true;
+    } else {
+        t1.reverse();
+        watch = false;
+    }
+});
+
+t1.to(
+    ".productmenu",
+    {
+        ease: "back.inOut(1.7)",
+        height: 198,
+        y: -10,
+    },
+    0
+);
+
+t1.to(
+    ".revealoffer",
+    {
+        ease: "power1.inOut",
+        bottom: 40,
+        opacity: 1,
+    },
+    0.5
+);
+t1.to(
+    ".offericon",
+    {
+        ease: "power1.inOut",
+        bottom: 0,
+        opacity: 1,
+    },
+    0.85
+);
